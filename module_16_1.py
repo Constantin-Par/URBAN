57ff1a1d3fe2936cdc97fa6ff01fa8f5a1b04ee4
Constantin-Par <constantin.parshukov@yandex.ru>
2024-12-20 09:51:58 +0300

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -9,17 +9,17 @@ async def root():
 
 
 @app.get("/user/admin")
-async def user_admin() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def user_admin() -> str:
+    return "Вы вошли как администратор"
 
 
 @app.get("/user")
-async def user_age(username: str, age: int) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+async def user_age(username: str, age: int) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
 
 
 @app.get("/user/{used_id}")
-async def user_id(used_id: int) -> dict:
-    return {"message": f"Вы вошли как пользователь № {used_id}"}
+async def user_id(used_id: int) -> str:
+    return f"Вы вошли как пользователь № {used_id}"
 
 # python -m uvicorn module_16_1:app --reload
